cbee29f6c7d1870503c623a7f794c8a7cf11540b
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-11-07 15:31:45 -0500

    Implemented Login Features

--- AnonChats/AnonChats/View Controllers/LoginViewController.swift
+++ AnonChats/AnonChats/View Controllers/LoginViewController.swift
@@ -6,24 +6,54 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        curveButtonEdges()
+        didTapBackground()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func curveButtonEdges() {
+        self.loginButton.layer.cornerRadius = 15
+        self.loginButton.layer.borderWidth = 2
     }
-    */
-
+    
+    func loginUser() {
+        PFUser.logInWithUsername(inBackground: usernameField.text!, password: passwordField.text!) { (success, error) in
+            if (success != nil) {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error description: \(String(describing: error?.localizedDescription))")
+            }
+        }
+    }
+    
+    @objc func backgroundTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        self.view.endEditing(true)
+    }
+    
+    func didTapBackground() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action:                           #selector(backgroundTapped(tapGestureRecognizer:)))
+        self.backgroundImage.addGestureRecognizer(tapGestureRecognizer)
+        self.backgroundImage.isUserInteractionEnabled = true
+    }
+    
+    @IBAction func didTapLogin(_ sender: Any) {
+        loginUser()
+    }
+    
+    @IBAction func didTapSignup(_ sender: Any) {
+        self.performSegue(withIdentifier: "signupSegue", sender: nil)
+    }
+    
 }
