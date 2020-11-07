a033a8c16f3934e9007e53331a1534892541fb11
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-11-07 13:10:02 -0500

    Project Template

---
+++ AnonChats/AnonChats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnonChats
+//
+//  Created by Subomi Popoola on 11/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
